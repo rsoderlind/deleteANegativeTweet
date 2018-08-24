b75823db54b8750a22496a322c31c9e9c878ef95
vagrant <vagrant@vagrant.vm>
2018-08-24 21:53:35 +0000

    My first version of a sentiment analysis tool to delete a tweet

---
+++ deleteANegativeTweet.py
@@ -0,0 +1,66 @@
+from credentials import *    # This will allow us to use the keys as variables
+import tweepy
+import re
+
+from textblob import TextBlob
+# We import our access keys:
+
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+
+# We create an extractor object:
+extractor = twitter_setup()
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+
+# We create a tweet list as follows:
+tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+
+# We print the most recent 5 tweets:
+print("5 recent tweets:\n")
+for tweet in tweets[:5]:
+    print(tweet.text)
+    print()
+
+    theTweet = clean_tweet(tweet.text)
+    theSentiment = analize_sentiment(theTweet)
+
+    print("The sentiment of this tweet is ", theSentiment)
+
+
+
+
+
